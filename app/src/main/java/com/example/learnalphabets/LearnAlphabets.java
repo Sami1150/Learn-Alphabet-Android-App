647d7e20a0fa61aeee846508b4c94817b27df116
Engr Sami Ahmad <bsef19m045@pucit.edu.pk>
2022-05-23 10:05:59 +0500

    Learn Alphabets xml

---
+++ app/src/main/java/com/example/learnalphabets/LearnAlphabets.java
@@ -0,0 +1,14 @@
+package com.example.learnalphabets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearnAlphabets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learn_alphabets);
+    }
+}
