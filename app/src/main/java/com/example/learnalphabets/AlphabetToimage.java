05dbf933769728d34cf34cee98c705e6de4a5211
Engr Sami Ahmad <bsef19m045@pucit.edu.pk>
2022-05-23 09:49:19 +0500

    Outline page from Images

---
+++ app/src/main/java/com/example/learnalphabets/AlphabetToimage.java
@@ -0,0 +1,14 @@
+package com.example.learnalphabets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlphabetToimage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alphabet_toimage);
+    }
+}
